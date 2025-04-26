24808ee111e6a88e43581d047d4741bdc6abbda2
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <fernandoagustinservinsanches@gmail.com>
2025-04-26 14:15:02 -0600

    Add files via upload

---
+++ plugins/main-staff copy.js
@@ -0,0 +1,24 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let img = './src/catalogo.jpg'
+let staff = `ᥫ᭡ *EQUIPO DE AYUDANTES* ❀
+✰ *Dueño* ${creador}
+✦ *Bot:* ${botname}
+⚘ *Versión:* ${vs}
+❖ *Libreria:* ${libreria} ${baileys}
+
+❍ *Creador:*
+
+ᰔᩚ 𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔
+> 🜸 Rol » *Creador*
+> ✧ GitHub » https://github.com/Fer280809
+
+`
+await conn.sendFile(m.chat, img, 'yuki.jpg', staff.trim(), fkontak)
+}
+  
+handler.help = ['staff']
+handler.command = ['colaboradores', 'staff']
+handler.register = true
+handler.tags = ['main']
+
+export default handler
